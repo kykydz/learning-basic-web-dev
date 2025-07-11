65a2bae1de894bc5ad30d362e78c1a5cadad2237
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-07-11 15:24:21 +0700

    fix: soal 7 buggy server api async-error

--- buggy-server/askjdahsdkj87364hhur2.js
+++ buggy-server/askjdahsdkj87364hhur2.js
@@ -1,6 +1,3 @@
 exports.s1h21782hd12yxywiuq = (req, res) => {
-    if (!req.body.error) {
-        return res.status(500).send("Internal Server Error");
-    }
-    res.status(200).send({ message: req.body.error });
+    res.status(200).send({ message: "hello ahmad rizal" });
 };
