11837b584abf7d23949bc9f3d8213fc2fb010f9f
Hizkiasugondo <hizkiaimanuel865@gamil.com>
2025-07-11 15:24:03 +0700

    Buggy

---
+++ Hizkia_231051001/buggy_server/askjdahsdkj87364hhur2.js
@@ -0,0 +1,6 @@
+export const s1h21782hd12yxywiuq = (req, res) => {
+  if (req.body.error) {
+    return res.status(500).send("Internal Server Error");
+  }
+  res.status(200).send({ message: "No error received" });
+};
---
+++ Hizkia_231051001/buggy_server/buggy-server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json()); 
+
+
+const { dhi237yh72384y38iririri } = require("./uyeuwq6623838-8234rur");
+
+app.post("/count", dhi237yh72384y38iririri);
+
+app.post("/async-error", async (req, res) => {
+  try {
+    const result = await Promise.reject("Oops!");
+    res.send(result);
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
